7b10d9cb7cddfeb61127fb67404ee7a853d32b32
Rishabh Gupta <guptarishabh97@gmail.com>
2019-06-27 17:17:03 +0530

    grab screen

---
+++ vport_backend/src/main/java/com/vport/Service/GrabScreenService.java
@@ -0,0 +1,35 @@
+package com.vport.Service;
+
+import org.springframework.stereotype.Service;
+import se.vidstige.jadb.JadbConnection;
+import se.vidstige.jadb.JadbDevice;
+import se.vidstige.jadb.JadbException;
+import se.vidstige.jadb.RemoteFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class GrabScreenService {
+
+    public byte[] grabScreen() throws IOException, JadbException {
+        JadbConnection jadbConnection = new JadbConnection();
+        JadbDevice device = (JadbDevice) jadbConnection.getDevices().get(0);
+
+        device.executeShell("screencap -p /sdcard/screencap.png");
+        device.pull(new RemoteFile("/sdcard/screencap.png"), new File("screencap.png"));
+
+        BufferedImage bImage = ImageIO.read(new File("screencap.png"));
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(bImage, "png", bos );
+        byte [] data = bos.toByteArray();
+
+
+        return data;
+
+    }
+}
