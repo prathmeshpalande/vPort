2b2b1fead2fcb74ee99e8d8202bf98323e0b7729
Rishabh Gupta <guptarishabh97@gmail.com>
2019-06-28 02:45:10 +0530

    swipe controller

---
+++ vport_backend/src/main/java/com/vport/Service/SwipeService.java
@@ -0,0 +1,26 @@
+package com.vport.Service;
+
+import com.vport.model.Coordinate;
+import org.springframework.stereotype.Service;
+import se.vidstige.jadb.JadbConnection;
+import se.vidstige.jadb.JadbDevice;
+import se.vidstige.jadb.JadbException;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class SwipeService {
+
+    public void processCoordinates(List<Coordinate> coordinateList) throws IOException, JadbException {
+        JadbConnection jadbConnection = new JadbConnection();
+        JadbDevice device = jadbConnection.getDevices().get(0);
+
+        float x1 = coordinateList.get(0).getX() * 1080;
+        float y1 = coordinateList.get(0).getY() * 1920;
+        float x2 = coordinateList.get(1).getX() * 1080;
+        float y2 = coordinateList.get(1).getY() * 1920;
+        device.executeShell( "touchscreen swipe " + x1 + " " + y1 + " " + x2 + " " + y2);
+        return;
+    }
+}
